80e5b985514e1e8ef0fae0f2dbe2bee85e98d4e1
Pedro Karasudani <pedro.karasudani@hotmail.com>
2025-02-16 20:39:11 -0300

    first commit

---
+++ order-item/src/main/java/br/com/pefacil/order_item/OrderItemApplication.java
@@ -0,0 +1,13 @@
+package br.com.pefacil.order_item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderItemApplication.class, args);
+	}
+
+}
