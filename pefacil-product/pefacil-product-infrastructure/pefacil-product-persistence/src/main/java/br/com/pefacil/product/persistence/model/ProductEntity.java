cff3461035b6946dd49f510c9f655b8f0c17b087
Pedro Karasudani <pedro.karasudani@hotmail.com>
2025-02-20 00:35:09 -0300

    feature: Structuring the models (ProductDTO, Product, ProductEntity)

--- pefacil-product/pefacil-product-infrastructure/pefacil-product-persistence/src/main/java/br/com/pefacil/product/persistence/model/ProductEntity.java
+++ pefacil-product/pefacil-product-infrastructure/pefacil-product-persistence/src/main/java/br/com/pefacil/product/persistence/model/ProductEntity.java
@@ -1,4 +1,24 @@
 package br.com.pefacil.product.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double quantityStoke;
+    private Double price;
+
 }
+
