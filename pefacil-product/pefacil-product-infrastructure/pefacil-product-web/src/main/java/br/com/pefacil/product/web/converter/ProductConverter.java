cc8c5e0e31e989d3ac772d8525cb7ae9d2c07dd9
Pedro Karasudani <pedro.karasudani@hotmail.com>
2025-02-21 12:57:10 -0300

    feature: creating the toDTO and toDomain method in ProductConverter

--- pefacil-product/pefacil-product-infrastructure/pefacil-product-web/src/main/java/br/com/pefacil/product/web/converter/ProductConverter.java
+++ pefacil-product/pefacil-product-infrastructure/pefacil-product-web/src/main/java/br/com/pefacil/product/web/converter/ProductConverter.java
@@ -6,10 +6,20 @@ import br.com.pefacil.product.web.model.ProductDTO;
 public class ProductConverter {
 
     public Product toDomain(ProductDTO productDTO) {
-        return new Product();
-    };
+        return Product.builder()
+                .id(productDTO.getId())
+                .name(productDTO.getName())
+                .description(productDTO.getDescription())
+                .quantityStoke(productDTO.getQuantityStoke())
+                .price(productDTO.getPrice()).build();
+    }
 
     public ProductDTO toDTO(Product product) {
-        return new ProductDTO();
+        return ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .quantityStoke(product.getQuantityStoke())
+                .price(product.getPrice()).build();
     }
 }
