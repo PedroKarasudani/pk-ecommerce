15470299abdac37c6a3173d871a562856c8807e1
Pedro Karasudani Desktop <pedro.karasudani@hotmail.com>
2025-02-25 00:29:32 -0300

    feature: add JPA

--- pefacil-product/pefacil-product-infrastructure/pefacil-product-persistence/src/main/java/br/com/pefacil/product/persistence/ProductRepository.java
+++ pefacil-product/pefacil-product-infrastructure/pefacil-product-persistence/src/main/java/br/com/pefacil/product/persistence/ProductRepository.java
@@ -1,50 +1,10 @@
 package br.com.pefacil.product.persistence;
 
-import br.com.pefacil.product.domain.exceptions.ProductNotFoundException;
-import br.com.pefacil.product.domain.model.Product;
-import br.com.pefacil.product.domain.port.spi.ProductPort;
 import br.com.pefacil.product.persistence.model.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
-
 @Repository
-public class ProductRepository implements ProductPort {
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
-    private final Map<Long, ProductEntity> productMap = new ConcurrentHashMap<>();
-    private final AtomicLong idGenerator = new AtomicLong(1);
-
-    @Override
-    public Product create(Product product) {
-        Long id = idGenerator.getAndIncrement();
-        ProductEntity productEntity = ProductEntity.fromDomain(product);
-        productMap.put(id, productEntity);
-        return productEntity.toDomain();
-    }
-
-    @Override
-    public Product findById(Integer id) {
-        ProductEntity productEntity = productMap.get(id.longValue());
-        if (productEntity == null) {
-            throw new ProductNotFoundException("Product with id" + id.longValue() + " was not found");
-        }
-        return productEntity.toDomain();
-    }
-
-    @Override
-    public Product update(Product product, Integer id) {
-        if (product.getId() == null || !productMap.containsKey(product.getId())) {
-            throw new ProductNotFoundException("Product not found");
-        }
-        ProductEntity productEntity = ProductEntity.fromDomain(product);
-        productMap.put(product.getId(), productEntity);
-        return productEntity.toDomain();
-    }
-
-    @Override
-    public void deleteById(Integer id) {
-        productMap.remove(id.longValue());
-    }
 }
