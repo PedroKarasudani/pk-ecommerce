05521776ed806896906df5a1fa7fec1c21f28485
Pedro Karasudani Desktop <pedro.karasudani@hotmail.com>
2025-02-25 00:41:32 -0300

    feature: created ProductReposityAdapter

---
+++ pefacil-product/pefacil-product-infrastructure/pefacil-product-persistence/src/main/java/br/com/pefacil/product/persistence/ProductRepositoryAdapter.java
@@ -0,0 +1,42 @@
+package br.com.pefacil.product.persistence;
+
+import br.com.pefacil.product.domain.model.Product;
+import br.com.pefacil.product.domain.port.spi.ProductPort;
+import br.com.pefacil.product.persistence.model.ProductEntity;
+
+public class ProductRepositoryAdapter implements ProductPort {
+
+    private final ProductRepository productRepository;
+
+    public ProductRepositoryAdapter(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product create(Product product) {
+        ProductEntity productEntity = ProductEntity.fromDomain(product);
+        productEntity = productRepository.save(productEntity);
+        return productEntity.toDomain();
+    }
+
+    @Override
+    public Product findById(Integer id) {
+        return productRepository.findById(id.longValue()).get().toDomain();
+    }
+
+    @Override
+    public Product update(Product newProduct, Integer id) {
+        ProductEntity newProductEntity = productRepository.findById(id.longValue()).get();
+        newProductEntity.setName(newProduct.getName());
+        newProductEntity.setDescription(newProduct.getDescription());
+        newProductEntity.setPrice(newProduct.getPrice());
+        newProductEntity.setQuantityStoke(newProduct.getQuantityStoke());
+        newProductEntity = productRepository.save(newProductEntity);
+        return newProductEntity.toDomain();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        productRepository.deleteById(id.longValue());
+    }
+}
