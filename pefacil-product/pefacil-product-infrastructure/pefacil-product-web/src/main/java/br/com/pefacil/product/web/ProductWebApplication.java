3c62c85f08368bbc5125aacb6105c7a70e6407d4
Pedro Karasudani Desktop <pedro.karasudani@hotmail.com>
2025-02-25 23:22:24 -0300

    feature: additional configuration for H2 functionality with JPA

--- pefacil-product/pefacil-product-infrastructure/pefacil-product-web/src/main/java/br/com/pefacil/product/web/ProductWebApplication.java
+++ pefacil-product/pefacil-product-infrastructure/pefacil-product-web/src/main/java/br/com/pefacil/product/web/ProductWebApplication.java
@@ -2,8 +2,12 @@ package br.com.pefacil.product.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "br.com.pefacil.product.persistence.model")
+@ComponentScan(basePackages = "br.com.pefacil.product")
 public class ProductWebApplication {
     public static void main(String[] args) {SpringApplication.run(ProductWebApplication.class, args);}
 }
