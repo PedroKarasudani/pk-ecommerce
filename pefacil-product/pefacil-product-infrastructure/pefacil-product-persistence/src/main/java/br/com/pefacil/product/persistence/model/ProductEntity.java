f7077a84fb5314b21fd24b27b47941d68cb5c1ed
Pedro Karasudani Desktop <pedro.karasudani@hotmail.com>
2025-02-25 23:46:49 -0300

    feature: add data.sql for test

--- pefacil-product/pefacil-product-infrastructure/pefacil-product-persistence/src/main/java/br/com/pefacil/product/persistence/model/ProductEntity.java
+++ pefacil-product/pefacil-product-infrastructure/pefacil-product-persistence/src/main/java/br/com/pefacil/product/persistence/model/ProductEntity.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "products")
+@Table(name = "tb_products")
 @Builder
 public class ProductEntity {
 
---
+++ pefacil-product/pefacil-product-infrastructure/pefacil-product-persistence/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_products (name, description, quantity_stoke, price) VALUES ('Lampada', '1200lms', 2, 22);
