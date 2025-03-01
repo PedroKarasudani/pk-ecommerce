f551dc19876f01974424dda8bfd8100a86550d58
Pedro Karasudani Desktop <pedro.karasudani@hotmail.com>
2025-03-01 10:58:36 -0300

    feature: delet DatabaseErrorException

--- pefacil-product/pefacil-product-infrastructure/pefacil-product-persistence/src/main/java/br/com/pefacil/product/persistence/exceptions/DatabaseErrorException.java
+++
@@ -1,7 +0,0 @@
-package br.com.pefacil.product.persistence.exceptions;
-
-public class DatabaseErrorException extends RuntimeException {
-  public DatabaseErrorException(String message) {
-    super("Error accessing the database: " + message);
-  }
-}
