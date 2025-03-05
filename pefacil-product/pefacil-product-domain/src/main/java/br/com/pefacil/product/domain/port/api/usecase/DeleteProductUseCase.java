a82f09a9dfc2b3058ea5e97fda34473bd06b8728
Pedro Karasudani <pedro.karasudani@hotmail.com>
2025-03-05 00:11:36 -0300

    feature: expection in DeleteProductUseCase

--- pefacil-product/pefacil-product-domain/src/main/java/br/com/pefacil/product/domain/port/api/usecase/DeleteProductUseCase.java
+++ pefacil-product/pefacil-product-domain/src/main/java/br/com/pefacil/product/domain/port/api/usecase/DeleteProductUseCase.java
@@ -1,5 +1,6 @@
 package br.com.pefacil.product.domain.port.api.usecase;
 
+import br.com.pefacil.product.domain.exceptions.ProductNotFoundException;
 import br.com.pefacil.product.domain.port.api.DeleteProduct;
 import br.com.pefacil.product.domain.port.spi.ProductPort;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ public class DeleteProductUseCase implements DeleteProduct {
 
     @Override
     public void deleteById(Integer id) {
+        this.port.findById(id).orElseThrow(() -> new ProductNotFoundException(id.longValue()));
         this.port.deleteById(id);
     }
 }
