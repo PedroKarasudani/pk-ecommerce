581519347d829b5e680274d723bcca222e8173b5
Pedro Karasudani Desktop <pedro.karasudani@hotmail.com>
2025-03-22 00:07:15 -0300

    feature: GATEWAY, configuration

--- pefacil-product/pefacil-product-infrastructure/pefacil-product-web/src/main/java/br/com/pefacil/product/web/ProductWebApplication.java
+++ pefacil-product/pefacil-product-infrastructure/pefacil-product-web/src/main/java/br/com/pefacil/product/web/ProductWebApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 @EntityScan(basePackages = "br.com.pefacil.product.persistence.model")
 @ComponentScan(basePackages = "br.com.pefacil.product")
 @EnableJpaRepositories(basePackages = "br.com.pefacil.product.persistence")
